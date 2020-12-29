a7c18c45d0932a9bacfa1155ccb89a12b8b140c1
zhinaoye <1061822116@qq.com>
2020-12-29 17:24:04 +0800

    事务管理入门

---
+++ SpringBoot-Data/Affair/src/main/java/com/example/affair/AffairApplication.java
@@ -0,0 +1,13 @@
+package com.example.affair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AffairApplication.class, args);
+    }
+
+}
